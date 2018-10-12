ceaa7e1b2d9ae7eb69f773957614a1278822ba76
kullu <kullu@kuldy>
2018-10-12 16:38:31 -0700

    maven project push demo 1

---
+++ src/test/java/com/vtiger/login/MavenDemo2.java
@@ -0,0 +1,29 @@
+package com.vtiger.login;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MavenDemo2 {
+	
+	
+	public static  int a=10;
+	int b= 20;
+
+		
+	@Test
+	public void add() {
+		System.out.println("SUM");
+		Assert.assertEquals(30, a+b);
+	}
+	@Test
+	public void sub() {
+		System.out.println("SUB = "+a);
+		Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void mul() {
+		System.out.println("MUL");
+		Assert.assertEquals(200, a*b);
+	}
+
+}
