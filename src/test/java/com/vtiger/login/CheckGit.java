9c3ac6675b8dbc7aaac78a48429c1eb4b2a8b158
kullu <kullu@kuldy>
2018-12-20 20:06:01 -0800

    second comit

---
+++ src/test/java/com/vtiger/login/CheckGit.java
@@ -0,0 +1,13 @@
+package com.vtiger.login;
+
+import org.testng.annotations.Test;
+
+public class CheckGit {
+	
+	
+	@Test
+	public void print() {
+		System.out.println("hi second commit");
+	}
+
+}
